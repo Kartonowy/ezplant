2df59451a51358abf44592272e51d9919b9a8847
Kartonowy <ttrollface.com.pl@gmail.com>
2025-03-12 10:33:46 +0100

    Card

--- app/src/main/java/com/plant/ezplant/MainActivity.kt
+++ app/src/main/java/com/plant/ezplant/MainActivity.kt
@@ -11,6 +11,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import com.plant.ezplant.ui.theme.EZplantTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,17 +44,22 @@ class MainActivity : ComponentActivity() {
 fun MainLayout(spelar: String = "dota",modifier: Modifier = Modifier) {
     Scaffold(
         bottomBar = { Navbar() }
-    ) { innerPadding ->
-        LazyColumn {
-            items(5) { index ->
-                Text(modifier = Modifier.padding(innerPadding),
-                    text = "Oh spelar lite ${spelar}")
-            }
-        }
+    ) { i ->
+        Tiles()
     }
 }
 
 @Composable
-fun Tile(modifier: Modifier = Modifier) {
-
+fun Tiles(modifier: Modifier = Modifier) {
+    LazyVerticalStaggeredGrid(
+        columns = StaggeredGridCells.Adaptive(200.dp),
+        verticalItemSpacing = 4.dp,
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        content = {
+            items(GivePlants()) { plant ->
+                PlantTile(plant)
+            }
+        },
+        modifier = Modifier.fillMaxSize()
+    )
 }
