a3b2e06cd553932e096b2ade22c27fc2ff2bb70a
Kartonowy <ttrollface.com.pl@gmail.com>
2025-04-17 22:23:44 +0200

    Updated & tiles

--- app/src/main/java/com/plant/ezplant/MainActivity.kt
+++ app/src/main/java/com/plant/ezplant/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,6 +19,10 @@ import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.AlignmentLine
@@ -42,16 +47,19 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun MainLayout(spelar: String = "dota",modifier: Modifier = Modifier) {
+
+
     Scaffold(
         bottomBar = { Navbar() }
-    ) { i ->
-        Tiles()
+    ) { innerPadding ->
+        Tiles(innerPadding, Modifier)
     }
 }
 
 @Composable
-fun Tiles(modifier: Modifier = Modifier) {
+fun Tiles(paddingValues: PaddingValues, modifier: Modifier) {
     LazyVerticalStaggeredGrid(
+        contentPadding = paddingValues,
         columns = StaggeredGridCells.Adaptive(200.dp),
         verticalItemSpacing = 4.dp,
         horizontalArrangement = Arrangement.spacedBy(4.dp),
