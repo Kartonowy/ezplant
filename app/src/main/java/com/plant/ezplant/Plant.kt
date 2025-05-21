b7e499743fd3df93e772f7d4149d217193e11e55
Kartonowy <ttrollface.com.pl@gmail.com>
2025-05-21 10:20:44 +0200

    Type converters

--- app/src/main/java/com/plant/ezplant/Plant.kt
+++ app/src/main/java/com/plant/ezplant/Plant.kt
@@ -1,7 +1,10 @@
 package com.plant.ezplant
 
+import android.graphics.Color
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
--- app/src/main/java/com/plant/ezplant/api/Database.kt
+++ app/src/main/java/com/plant/ezplant/api/Database.kt
@@ -3,10 +3,13 @@ package com.plant.ezplant.api
 import android.content.Context
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.plant.ezplant.api.daos.PlantDao
+import com.plant.ezplant.api.entities.Converters
 import com.plant.ezplant.api.entities.PlantEntity
 
 @androidx.room.Database(entities = [PlantEntity::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class Database : RoomDatabase() {
     abstract fun PlantDao(): PlantDao
 
