e0644db8ccd31d3b65584a8d8eaccfa836d6bd30
lacertavirdis <zuzanna.przybyl@uczen.zsk.poznan.pl>
2025-05-23 22:10:06 +0200

    homepage style

--- app/src/main/java/com/plant/ezplant/MainActivity.kt
+++ app/src/main/java/com/plant/ezplant/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -28,11 +29,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.room.Room
+import com.plant.ezplant.ui.modifiers.backgroundTiledImage
 import com.plant.ezplant.ui.theme.EZplantTheme
 import com.plant.ezplant.viewmodels.PlantViewModel
 
@@ -63,37 +69,53 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun MainLayout(spelar: String = "dota", modifier: Modifier = Modifier) {
-
     var currentScreen = remember { mutableStateOf(Screen.Home) }
 
-    Scaffold(
-        bottomBar = { Navbar(currentScreen, Modifier) }
-    ) { innerPadding ->
-        when (currentScreen.value) {
-            Screen.Home -> Tiles(innerPadding, Modifier)
-            Screen.Search -> Text("Search")
-            Screen.Add -> Text("Add")
-            Screen.List -> Text("List")
-            Screen.Profile -> Text("Profile")
+    val imagePainter = painterResource(id = R.drawable.potted_plant2)
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .backgroundTiledImage(imagePainter)
+    ) {
+        Scaffold(
+            bottomBar = { Navbar(currentScreen, Modifier) }
+        ) { innerPadding ->
+            when (currentScreen.value) {
+                Screen.Home -> Tiles(innerPadding, Modifier)
+                Screen.Search -> Text("Search")
+                Screen.Add -> Text("Add")
+                Screen.List -> Text("List")
+                Screen.Profile -> Text("Profile")
+            }
         }
     }
 }
+
 
 @Composable
 fun Tiles(paddingValues: PaddingValues, modifier: Modifier) {
     val vm = PlantViewModel(Database.getInstance(MainActivity.appContext).PlantDao())
 
+    val backgroundPainter = painterResource(R.drawable.potted_plant2)
+
     val plants by vm.plants.collectAsState()
-    LazyVerticalStaggeredGrid(
-        contentPadding = paddingValues,
-        columns = StaggeredGridCells.Adaptive(200.dp),
-        verticalItemSpacing = 4.dp,
-        horizontalArrangement = Arrangement.spacedBy(4.dp),
-        content = {
-            items(plants) { plant ->
-                PlantTile(plant)
-            }
-        },
-        modifier = Modifier.fillMaxSize()
-    )
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .backgroundTiledImage(backgroundPainter)
+    ) {
+        LazyVerticalStaggeredGrid(
+            contentPadding = paddingValues,
+            columns = StaggeredGridCells.Adaptive(200.dp),
+            verticalItemSpacing = 4.dp,
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            content = {
+                items(plants) { plant ->
+                    PlantTile(plant)
+                }
+            },
+            modifier = Modifier.fillMaxSize()
+        )
+    }
 }
--- app/src/main/java/com/plant/ezplant/Plant.kt
+++ app/src/main/java/com/plant/ezplant/Plant.kt
@@ -1,28 +1,68 @@
 package com.plant.ezplant
 
+import android.graphics.drawable.Drawable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.plant.ezplant.entities.PlantEntity
+import com.plant.ezplant.ui.modifiers.cardStyle
+import com.plant.ezplant.ui.theme.GreenOnPrimary
+import com.plant.ezplant.ui.theme.GreenPrimary
+
 
 @Composable
 public fun PlantTile(plant: PlantEntity, modifier: Modifier = Modifier) {
     Card(
-        modifier = Modifier.width(10.dp)
+        modifier = Modifier.cardStyle(),
+        colors = CardColors(GreenPrimary, GreenOnPrimary, GreenPrimary, GreenPrimary)
     ) {
-        Text(
-            text = plant.plantName.toString(),
-            fontSize = 32.sp
-        )
-        Text(
-            text = "Is it dead? ${plant.dehydration}",
-            fontSize = 18.sp
-        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+
+                Image(
+                    modifier = Modifier
+                        .size(90.dp)
+                        .padding(5.dp),
+                    painter = painterResource(id = R.drawable.placeholder_plant),
+                    contentDescription = "Plant"
+                )
+
+                Text(
+                    text = plant.plantName.toString(),
+                    fontSize = 25.sp,
+                    fontFamily = FontFamily.SansSerif
+                )
+                Text(
+                    text = "Is it dead? ${plant.dehydration}",
+                    fontSize = 18.sp,
+                    fontFamily = FontFamily.Monospace
+                )
+            }
+
+        }
+
     }
 }
