422565a5ca6383623a507a7adf00588db4616a4b
lacertavirdis <zuzanna.przybyl@uczen.zsk.poznan.pl>
2025-05-26 17:29:32 +0200

    styled add plant

--- app/src/main/java/com/plant/ezplant/ui/theme/Color.kt
+++ app/src/main/java/com/plant/ezplant/ui/theme/Color.kt
@@ -10,9 +10,20 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+val Background = Color(0x50f8f0f4)
+
 val GreenPrimary = Color(0xFFC8E6C9)
 val GreenOnPrimary = Color(0xFF000000)
-
 val GreenBorder = Color(0xFFA5D6A7)
 
 val AddButton = Color(0xFFe6c8d6)
+val AddButtonBorder = Color(0xFFdeb6c9)
+val AddOnButton = Color(0xFF783755)
+
+val PhotoButton = Color(0xFFd8c8e6)
+val PhotoButtonBorder = Color(0xFFccb6de)
+val PhotoOnButton = Color(0xFF5a3778)
+
+val PlantNameButton = Color(0xFFd2e1f1)
+val PlantNameButtonBorder = Color(0xFFbfd4eb)
+val PlantNameOnButton = Color(0xFF295382)
